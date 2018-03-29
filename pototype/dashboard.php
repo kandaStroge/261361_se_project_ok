ca52176b4636aaf7f462abe61f312739cf38b3d0
Kanokwan Pinthong <kwpinthong@users.noreply.github.com>
2018-03-29 21:27:28 +0700

    edit dashboard name

--- pototype/dashboard.php
+++ pototype/dashboard.php
@@ -5,9 +5,9 @@
         exit(0);
     }
     if($_SESSION['token'] == "TEACHER"){
-        include_once 'inc/teacher_drashboard.php';
+        include_once 'inc/teacher_dashboard.php';
     }elseif($_SESSION['token'] == "STUDENT"){
-        include_once 'inc/student_drashboard.php';
+        include_once 'inc/student_dashboard.php';
     }else{
         echo "error token";
     }
